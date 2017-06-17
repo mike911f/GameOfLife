060f29559301457fe97555da812b6ad758097fca
Martin Michalko <matemichal@gmail.com>
2017-06-17 13:27:57 +0200

    state changing for living and dead cell

--- life.py
+++ life.py
@@ -10,10 +10,19 @@ class Game:
 
 		for line in range(self.height):
 			for cell in range(self.width):
+				#Any live cell
 				if self.state[line][cell] == 1:
-					neighbours_number=
-
-				#    Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
-				#    Any live cell with two or three live neighbours lives on to the next generation.
-				#    Any live cell with more than three live neighbours dies, as if by overpopulation.
-				#    Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+					#with fewer than two live neighbours dies, as if caused by underpopulation.
+					if self.get_neighbours_number(line,cell)<2:
+						self.new_state[line][cell] = 0
+					# with two or three live neighbours lives on to the next generation.
+					if self.get_neighbours_number(line,cell)>=2 and \
+						self.get_neighbours_number(line,cell)<=3:
+						self.new_state[line][cell] = 1
+					#with more than three live neighbours dies, as if by overpopulation.
+					if self.get_neighbours_number(line,cell)>3:
+						self.new_state[line][cell] = 0
+				else:
+				# Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+					if self.get_neighbours_number(line,cell)=3:
+						self.new_state[line][cell] = 1
