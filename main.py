a82ecabf94bb60f2ac4d5c758b285e5423d4a168
mike911f <michaeljurek@seznam.cz>
2017-06-17 14:24:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 
+from graphics import *
+import random
+import life
+
+
+
+def main():
+    win_max = 750
+
+    win = GraphWin("Game Of Life", win_max, win_max)
+    
+    obj = life.Game(win_max, win_max)
+    obj.rand_state()
+    
+    while True:
+        obj.step()
+        for line in range(obj.height):
+			    for cell in range(obj.width):
+				
+				    if obj.state[line][cell] == 0 :
+				    	win.plotPixel(line, cell, color="white")
+				    else :
+				    	win.plotPixel(line, cell, color="black")
+
+    win.getMouse()
+    win.close()
+
+
+main()
